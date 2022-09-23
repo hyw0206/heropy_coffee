e4ff49990bf3f52b50781e50e9964881afa0f110
hyw0206 <youngwookheo0206@gmail.com>
2022-09-23 11:10:45 +0900

    코드 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-// 요소 찾기
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-// 검색창 요소 클릭시 실행
-searchInputEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-// input 요소에 포커스 되면 실행
-// focused class를 추가 / placeholder 속성에 통합검색 값을 지정
-searchInputEl.addEventListener('focus',function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-// input 요소에 focus가 blur (해제) 되면 실행
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  // focus가 blur 되면 값을 지움
-  searchInputEl.value = null;
-  searchInputEl.setAttribute('placeholder','');
-});
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 // scrollY -> Y축으로 얼마나 스크롤했는지 체크
@@ -130,5 +110,4 @@ new Swiper('.awards .swiper', {
     nextEl: '.awards .swiper-button-next'  // 다음 버튼
   }
 });
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
+
